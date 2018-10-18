b7bca76fbfe2efe4a52a95320f2f506561945bd9
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-10-18 14:39:18 +0200

    Move out custom android code
    It looks like the android directory is used in newer versions of Cordova.

--- hooks/before_prepare/java_copy.sh
+++ hooks/before_prepare/java_copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ -e ./platforms/android ] && cp android/MainActivity.java ./platforms/android/app/src/main/java/io/cordova/hellocordova/MainActivity.java
+[ -e ./platforms/android ] && cp android_custom/MainActivity.java ./platforms/android/app/src/main/java/io/cordova/hellocordova/MainActivity.java
 exit 0
